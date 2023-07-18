b6ed865fedc60acdc0790ca8752462b5621112de
Your Name <you@example.com>
2023-07-17 22:42:16 -0700

    New commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
  * main -it prints the size of the types of data
  *
- * Return: alwasys (0)
+ * Return: always (0)
  *
  */
 int main(void)
@@ -13,10 +13,10 @@ int main(void)
 	long long int d;
 	float e;
 
-	printf("size of a char: %lu\n", sizeof(a));
-	printf("size of int: %lu\n", sizeof(b));
-	printf("size of long int: %lu\n", sizeof(c));
-	printf("size of long long int: %lu\n", sizeof(d));
-	printf("size of float: %lu\n", sizeof(e));
+	printf("size of a char: %lu byte(s)\n", sizeof(a));
+	printf("size of int: %lu byte(s)\n", sizeof(b));
+	printf("size of long int: %lu byte(s)\n", sizeof(c));
+	printf("size of long long int: %lu byte(s)\n", sizeof(d));
+	printf("size of float: %lu byte(s)\n", sizeof(e));
 	return (0);
 }
