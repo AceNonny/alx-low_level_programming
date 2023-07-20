495cb5996a589f0e47337ecbbafc09b5e4604e2f
Your Name <you@example.com>
2023-07-19 23:29:46 -0700

    My late Nested loop

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,9 +1,11 @@
+#include "main.h"
+#include <unistd.h>
 /**
  * _putchar - it writes the character c to stdout
  * @c: The character to print
  *
  * Return: Success 1.
- * Error, -1 is returned, and errno is set appropriately.
+ * On error, -1 is returned, and errno is set appropriately.
 */
 int _putchar(char c)
 {
