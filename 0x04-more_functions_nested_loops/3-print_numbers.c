3f9c316309e07ce7a9165e1740594cf841eb6a96
Your Name <you@example.com>
2023-07-20 15:52:52 -0700

    New coding in peinting 0 to 9 with c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - It prints numbers from 0 to 9
+ * Return: The numbers from 0 to 9
+*/
+
+void print_numbers(void)
+{
+
+	int c;
+
+	for (c >= 48; c <= 57; c++)
+	{
+	return (c + '0');
+	}
+	return ('\n');
+}
