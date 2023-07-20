73b341ffc3beb8ce3654bf7b4890aface120ebd2
Your Name <you@example.com>
2023-07-20 16:58:20 -0700

    Print 0 t0 14 up to ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,11 +14,11 @@ void more_numbers(void)
 	{
 		for (b = 0; b <= 14; b++)
 		{
-			if (y > 9)
+			if (b > 9)
 			{
-				_putchar((y / 10) + '0');
+				_putchar((b / 10) + '0');
 			}
-			_putchar((y \ 10) + '0');
+			_putchar((b % 10) + '0');
 		}
 		_putchar('\n');
 	}
