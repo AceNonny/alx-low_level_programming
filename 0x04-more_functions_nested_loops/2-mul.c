9b9375a96d5f55a116b79b2ad2b8af32b2b6dd78
Your Name <you@example.com>
2023-07-20 11:43:01 -0700

    My first multiplicaation coding with c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two digits
+ * @a: the first integer to be multiplied
+ * @b: the second integer that multiplies the first integer
+ * Return: The product of the multiplication of a and b
+*/
+
+int mul(int a, int b)
+{
+	return (a * y);
+}
