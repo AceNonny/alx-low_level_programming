ff314ec24bab556bad4ffd95ddf5bd9159707e15
Your Name <you@example.com>
2023-07-20 17:46:02 -0700

    Even squares too

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - it prints squares n number of times
+ * @size: the number of times that determines the line to be printed
+ * Return: Always 0
+*/
+
+void print_square(int size)
+
+{
+	int a, d;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (d = 0; d < size; a++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
