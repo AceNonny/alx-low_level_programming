ab30e3c68b6f7ff3420cd032bb090db4898933b2
Your Name <you@example.com>
2023-07-24 22:03:14 -0700

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - it copies a string
+ * @dest: the destination value
+ * @src: the source value
+ * Return: take the pointer to the destination
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; src[a] != '\0'; a++)
+	{
+		dest[a] = scr[a];
+	}
+	dest[a++] = '\0';
+	return (dest);
+}
