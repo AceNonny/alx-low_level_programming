08b57022ca66446b4d33a444d971e511d52ab723
AceNonny <chinonso1mbalewe@gmail.com>
2023-09-26 01:29:33 +0000

    Memeset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,7 +5,7 @@
  *    * @src: the string for filling
  *     * @n: the lenght of buffer
  *      * Return: new string.
- *       */
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int a = 0;
